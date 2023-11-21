ba6b3708295db861d21c88dc0719aa1ebf590717
rezkyputra16 <rezkyputra827@gmail.com>
2023-11-21 11:05:42 +0800

    tambah gambar

--- src/start/index.jsx
+++ src/start/index.jsx
@@ -6,37 +6,23 @@ function Start() {
       <Carousel.Item>
         <img
           style={{ display: "center", width: "950px" }}
-          src="https://markey.id/wp-content/uploads/2019/01/20190103.jpg"
+          src="./src/components/img/slide1.jpg"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          src="https://markey.id/wp-content/uploads/2019/01/20190103.jpg"
+          src="./src/components/img/slide2.jpg"
           alt="Second slide"
           style={{ display: "center", width: "950px" }}
         />
-        <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           style={{ display: "center", width: "950px" }}
-          src="https://markey.id/wp-content/uploads/2019/01/20190103.jpg"
+          src="./src/components/img/slide3.jpg"
           alt="Third slide"
         />
-        <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
